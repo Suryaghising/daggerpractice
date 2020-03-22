147fb451994a6de3e3e8f8263c344c769385b8cf
root <root@localhost.localdomain>
2020-03-22 13:59:54 +0545

    Added profile fragment

--- app/src/main/java/com/surya/daggerpractice/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/surya/daggerpractice/ui/main/profile/ProfileFragment.java
@@ -5,12 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.surya.daggerpractice.R;
+import com.surya.daggerpractice.models.User;
+import com.surya.daggerpractice.ui.auth.AuthResource;
 import com.surya.daggerpractice.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -20,6 +24,7 @@ import dagger.android.support.DaggerFragment;
 public class ProfileFragment extends DaggerFragment {
 
     private ProfileViewModel profileViewModel;
+    TextView emailText, nameText, websiteText;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -35,6 +40,46 @@ public class ProfileFragment extends DaggerFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        emailText = view.findViewById(R.id.emailId);
+        nameText = view.findViewById(R.id.nameId);
+        websiteText = view.findViewById(R.id.websiteId);
         profileViewModel = ViewModelProviders.of(this, providerFactory).get(ProfileViewModel.class);
+
+        subscribeObserver();
+
+    }
+
+    public void subscribeObserver() {
+        profileViewModel.getAuthUser().removeObservers(getViewLifecycleOwner());
+        profileViewModel.getAuthUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+
+                if (userAuthResource != null) {
+                    switch (userAuthResource.status) {
+                        case AUTHENTICATED:
+                            setUserDetail(userAuthResource.data);
+                            break;
+
+                        case ERROR:
+                            setErrorDetail(userAuthResource.message);
+                            break;
+
+                    }
+                }
+            }
+        });
+    }
+
+    private void setUserDetail(User user) {
+        emailText.setText(user.getEmail());
+        nameText.setText(user.getName());
+        websiteText.setText(user.getWebsite());
+    }
+
+    private void setErrorDetail(String message) {
+        emailText.setText(message);
+        nameText.setText("Error=====");
+        websiteText.setText("Error=====");
     }
 }
--- app/src/main/java/com/surya/daggerpractice/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/surya/daggerpractice/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.surya.daggerpractice.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.surya.daggerpractice.SessionManager;
+import com.surya.daggerpractice.models.User;
+import com.surya.daggerpractice.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,14 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
-        Log.d(TAG, "ProfileViewModel: Created profile viewmodel====");
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    public LiveData<AuthResource<User>> getAuthUser() {
+        return sessionManager.getUser();
     }
 }
